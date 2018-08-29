bcc63970ac38d6f655ee50226ccf104016f1d575
long <229690874@qq.com>
2018-08-29 11:42:24 +0800

    fragment换成v4

--- core/src/main/java/com/weexbox/core/controller/WBBaseFragment.kt
+++ core/src/main/java/com/weexbox/core/controller/WBBaseFragment.kt
@@ -1,7 +1,7 @@
 package com.weexbox.core.controller
 
-import android.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import com.weexbox.core.router.Router
 
 open class WBBaseFragment: Fragment() {
