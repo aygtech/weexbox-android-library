03f245a49cbf10afd65eceb47d48411bbf24c2c2
long <229690874@qq.com>
2018-08-31 11:35:03 +0800

    tttt

--- core/src/main/java/com/weexbox/core/controller/WBBaseFragment.kt
+++ core/src/main/java/com/weexbox/core/controller/WBBaseFragment.kt
@@ -10,7 +10,7 @@ open class WBBaseFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        router = savedInstanceState?.get(Router.extraName) as? Router
+        router = savedInstanceState?.get(Router.extraName) as Router?
     }
 
 }
