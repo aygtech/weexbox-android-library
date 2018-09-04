806fe3e494c0991f2922820fd059d730e8e63766
温天伟 <leon.wen@aiyuangong.com>
2018-09-04 15:19:15 +0800

    fragement EventBus

--- core/src/main/java/com/weexbox/core/controller/WBBaseFragment.kt
+++ core/src/main/java/com/weexbox/core/controller/WBBaseFragment.kt
@@ -3,14 +3,48 @@ package com.weexbox.core.controller
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.weexbox.core.router.Router
+import com.weexbox.core.util.EventBusUtil
 
 open class WBBaseFragment: Fragment() {
 
     var router: Router? = null
+    var mFrageMentTag = "";// fragement标识
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         router = arguments?.getSerializable(Router.extraName) as Router?
+        if (isRegisterEventFrageMent() && inCreateEventFrageMent()) {
+            EventBusUtil.register(this)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (isRegisterEventFrageMent() && !inCreateEventFrageMent()) {
+            EventBusUtil.register(this)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (isRegisterEventFrageMent() && !inCreateEventFrageMent()) {
+            EventBusUtil.unregister(this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isRegisterEventFrageMent() && inCreateEventFrageMent()) {
+            EventBusUtil.unregister(this)
+        }
+    }
+
+    fun isRegisterEventFrageMent(): Boolean {
+        return false
+    }
+
+    fun inCreateEventFrageMent(): Boolean {
+        return false
     }
 
 }
