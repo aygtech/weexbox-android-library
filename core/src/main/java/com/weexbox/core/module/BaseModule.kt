6d95b039724ea7b5eb2b8a763fbfb86a18c4ffd3
long <229690874@qq.com>
2018-11-15 14:22:48 +0800

    getfragment递归

--- core/src/main/java/com/weexbox/core/module/BaseModule.kt
+++ core/src/main/java/com/weexbox/core/module/BaseModule.kt
@@ -1,11 +1,9 @@
 package com.weexbox.core.module
 
 import android.support.v4.app.Fragment
-import com.alibaba.fastjson.JSONObject
 import com.taobao.weex.common.WXModule
 import com.weexbox.core.controller.WBBaseActivity
 import com.weexbox.core.controller.WBWeexFragment
-import java.util.*
 
 /**
  * Author: Mario
@@ -13,7 +11,7 @@ import java.util.*
  * Description: This is BaseModule
  */
 
-open class BaseModule: WXModule() {
+open class BaseModule : WXModule() {
 
     fun getActivity(): WBBaseActivity {
         return mWXSDKInstance.context as WBBaseActivity
@@ -21,20 +19,15 @@ open class BaseModule: WXModule() {
 
     fun getFragment(): WBWeexFragment? {
         val fragments = getActivity().supportFragmentManager.fragments as List<Fragment>
-        for (fragment in fragments) {
-            if (fragment is WBWeexFragment){
-                if (mWXSDKInstance == fragment.instance) {
-                    return fragment
-                }
-            } else if (fragment.childFragmentManager.fragments.size > 0){
-                val fragmentss = fragment.childFragmentManager.fragments as List<Fragment>
-                for (fragmentt in fragmentss){
-                    if (fragmentt is WBWeexFragment){
-                        if (mWXSDKInstance == fragmentt.instance) {
-                            return fragmentt
-                        }
-                    }
-                }
+        return getRecursionFragment(fragments)
+    }
+
+    private fun getRecursionFragment(fragments: List<Fragment>): WBWeexFragment? {
+        for (recursionFragment in fragments) {
+            if (recursionFragment is WBWeexFragment && mWXSDKInstance == recursionFragment.instance) {
+                return recursionFragment
+            } else if (recursionFragment.childFragmentManager.fragments.size > 0){
+                getRecursionFragment(recursionFragment.childFragmentManager.fragments)
             }
         }
         return null
