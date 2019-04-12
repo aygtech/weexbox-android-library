5c06594fc042a733d0a8439d2928420c46831b49
Mario <myeveryheart@qq.com>
2019-04-12 17:53:31 +0800

    abiFilters "armeabi-v7a"改abiFilters "arm64-v8a", "x86" ws

--- core/src/main/java/com/weexbox/core/controller/WBWeexFragment.kt
+++ core/src/main/java/com/weexbox/core/controller/WBWeexFragment.kt
@@ -17,7 +17,9 @@ import com.taobao.weex.common.WXRenderStrategy
 import com.weexbox.core.R
 import com.weexbox.core.WeexBoxEngine
 import com.weexbox.core.event.Event
+import com.weexbox.core.extension.appendingPathComponent
 import com.weexbox.core.update.UpdateManager
+import com.weexbox.core.util.HotReloadManager
 import java.io.IOException
 
 /**
@@ -30,6 +32,8 @@ open class WBWeexFragment : WBBaseFragment(), IWXRenderListener {
     var instance: WXSDKInstance? = null
     private var broadcastReceiver: BroadcastReceiver? = null
     private var isFirstSendDidAppear = true
+    private var url: String? = null
+    private var hotReloadManager: HotReloadManager? = null
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_weex
@@ -46,15 +50,15 @@ open class WBWeexFragment : WBBaseFragment(), IWXRenderListener {
         instance?.setRenderContainer(renderContainer)
         instance?.registerRenderListener(this)
         instance?.isTrackComponent = false
-        if (router.url != null) {
-            if (router.url!!.startsWith("http")) {
+        if (url != null) {
+            if (url!!.startsWith("http")) {
                 // 下载
-                instance?.renderByUrl(router.url, router.url, null, null, WXRenderStrategy.APPEND_ASYNC)
+                instance?.renderByUrl(url, url, null, null, WXRenderStrategy.APPEND_ASYNC)
             } else {
                 try {
-                    val file = UpdateManager.getFullUrl(router.url!!)
+                    val file = UpdateManager.getFullUrl(url!!)
                     val template = FileUtils.readFileToString(file)
-                    instance?.render(router.url, template, null, null, WXRenderStrategy.APPEND_ASYNC)
+                    instance?.render(url, template, null, null, WXRenderStrategy.APPEND_ASYNC)
                 } catch (e: IOException) {
                     Logger.e(e, "文件不存在")
                 }
@@ -67,12 +71,39 @@ open class WBWeexFragment : WBBaseFragment(), IWXRenderListener {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        url = router.url
+
+        if (WeexBoxEngine.isDebug) {
+            hotReloadManager = HotReloadManager(object : HotReloadManager.ActionListener {
+                override fun reload() {
+                    refreshWeex()
+                }
+
+                override fun render(bundleUrl: String) {
+                    if (url != null && isVisibleToUser) {
+                        var name = url!!
+                        if (url!!.startsWith("http")) {
+                            val before = url!!.substringBeforeLast("/")
+                            val after = url!!.substringAfterLast("/")
+                            name = before.substringAfterLast("/").appendingPathComponent(after)
+                        }
+                        if (bundleUrl.endsWith(name)) {
+                            url = bundleUrl
+                            refreshWeex()
+                        }
+                    }
+
+                }
+            })
+        }
+
         render()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
+        hotReloadManager?.destroy()
         instance?.destroy()
     }
 
@@ -151,7 +182,7 @@ open class WBWeexFragment : WBBaseFragment(), IWXRenderListener {
      * @return
      */
     fun getFragmentSimpleName(): String? {
-        return router.url ?: "WBWeexFragment"
+        return url ?: "WBWeexFragment"
     }
 
     override fun onBackPressed() {
