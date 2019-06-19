d7ecfe4c9fe4f0dd432c7cbb981681cfcbaeac13
Mario <myeveryheart@qq.com>
2019-06-19 16:30:04 +0800

    api 'com.taobao.android:weex_sdk:0.24.0' 删除richtext

--- core/src/main/java/com/weexbox/core/WeexBoxEngine.kt
+++ core/src/main/java/com/weexbox/core/WeexBoxEngine.kt
@@ -10,8 +10,6 @@ import com.taobao.weex.WXSDKEngine
 import com.weexbox.core.adapter.ImageAdapter
 import com.weexbox.core.adapter.WebSocketAdapterFactory
 import com.weexbox.core.component.LottieComponent
-import com.weexbox.core.component.RichTextComponent
-import com.weexbox.core.component.richtext.WXRichText
 import com.weexbox.core.controller.WBWebViewActivity
 import com.weexbox.core.controller.WBWeexActivity
 import com.weexbox.core.module.*
@@ -77,8 +75,6 @@ object WeexBoxEngine {
     }
 
     private fun registerComponent() {
-//        WXSDKEngine.registerComponent("wb-richtext", RichTextComponent::class.java)
-        WXSDKEngine.registerComponent("richtext", WXRichText::class.java)
         WXSDKEngine.registerComponent("wb-lottie", LottieComponent::class.java)
     }
 }
